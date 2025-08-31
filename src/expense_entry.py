f6124b5439d173b2f29d549139524eddd655cf24
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-30 21:05:56 -0400

    feat: ✨ Implemented function to gather user input for expenses
    Implemented function that will collect (and validate) expense details, including category, name, and amount. After data is validated, and confirmed by the user, the function returns the entered values.
    
    None

---
+++ src/expense_entry.py
@@ -0,0 +1,44 @@
+from main import CATEGORY
+
+def get_expense_detail():
+    '''Collects and returns expense details.
+    Parameters:
+        Category (represented by category index, or 'cidx'), must be entered as a number to be converted back to an integer
+        Expense name (represented by a string)
+        Expense amount (represented by an integer)
+    '''
+    for i, cat in enumerate(CATEGORY, 1):
+        print(f"{i}. {CATEGORY}")
+    
+    print("Enter the category number for this expense.")
+    cidx = int(input("> "))
+
+    try:
+        cidx = CATEGORY(cidx - 1)
+    except ValueError:
+        print("Invalid entry. Please enter the corresponding number.")
+        return None
+    
+    print("Enter the expense name.")
+    expense_name = str(input("> "))
+
+    print("Enter expense amount.")
+    while True:
+        try:
+            expense_amount = float(input("> $"))
+        except ValueError:
+            print("Invalid. Use only numbers and decimals.")
+            continue
+        break
+
+    print(f"Expense: {expense_name}, {expense_amount}, in {cidx}.")
+    print("Confirm expense? (y/n)")
+    confirm = str(input("> "))
+
+    if confirm == 'y':
+        return cidx, expense_name, expense_amount
+    elif confirm == 'n':
+        return None
+    else:
+        print("Invalid entry. Please enter 'y' or 'n'.")
+        return None
