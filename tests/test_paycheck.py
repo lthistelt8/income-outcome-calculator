97d5b80e82e986cf076918a9c5070272a7eb720b
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-31 17:05:24 -0400

    style: 🎨 Added docstrings for test module
    Test_paycheck now has a docstring, as well as the 'test_add_expense()' function.
    
    None

--- tests/test_paycheck.py
+++ tests/test_paycheck.py
@@ -1,8 +1,13 @@
+'''
+Contains test functions for source code + business logic
+'''
+
 from main import add_expense
 
 test_expenses = {}
 
 def test_add_expense():
+    '''Test the add_expense() function'''
     add_expense('Fixed', 'Mortgage', 90)
     assert 'Fixed' in test_expenses['category']
     assert 'Mortgage' in test_expenses['expense name']
