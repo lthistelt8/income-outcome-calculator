1bd8a27be672f9e9a319224f1a2a65533894af01
lthistelt8 <o_lthistel6@protonmail.com>
2025-09-01 11:17:59 -0400

    refactor: 🎨 Added correct import
    Added the correct import for main into test module

--- tests/test_paycheck.py
+++ tests/test_paycheck.py
@@ -1,14 +1,14 @@
 '''
 Contains test functions for source code + business logic
 '''
-
-from main import add_expense
-
-test_expenses = {}
+import src.main
 
 def test_add_expense():
     '''Test the add_expense() function'''
-    add_expense('Fixed', 'Mortgage', 90)
+
+    test_expenses = {}
+
+    src.main.add_expense('Fixed', 'Mortgage', 90)
     assert 'Fixed' in test_expenses['category']
     assert 'Mortgage' in test_expenses['expense name']
     assert 90 in test_expenses['expense amount']
