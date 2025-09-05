bd5a8441f0362d67e1c95f9f0b82494cbe542986
lthistelt8 <o_lthistel6@protonmail.com>
2025-09-05 11:45:02 -0400

    feat: initialized 'pathfinder' function to display working path for debugging

--- tests/test_paycheck.py
+++ tests/test_paycheck.py
@@ -1,17 +1,23 @@
 '''
 Contains test functions for source code + business logic
 '''
-import src.main
+from pathlib import Path as p
+import src.main as s
+
+def pathfinder():
+    '''Displays working path to test module'''
+    working_path = p(__file__).parent
+    print(f"\nWorking Directory: {working_path}")
 
 def test_add_expense():
     '''Test the add_expense() function'''
 
     test_expenses = {}
 
-    src.main.add_expense('Fixed', 'Mortgage', 90)
+    s.add_expense('Fixed', 'Mortgage', 90)
     assert 'Fixed' in test_expenses['category']
     assert 'Mortgage' in test_expenses['expense name']
     assert 90 in test_expenses['expense amount']
     print(test_expenses)
 
-test_add_expense()
+pathfinder()
