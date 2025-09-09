0b158592b38d86f20ff18092683a06e105f82cc1
lthistelt8 <o_lthistel6@protonmail.com>
2025-09-09 09:41:11 -0400

    style: renamed 'TestExpenses' class to 'TestExpense'

--- tests/test_paycheck.py
+++ tests/test_paycheck.py
@@ -5,7 +5,7 @@ import unittest as u
 import unittest.mock as um
 from src.main import expenses, add_expense, del_expense
 
-class TestExpenses(u.TestCase):
+class TestExpense(u.TestCase):
     '''Test business logic for expenses'''
 
     def test_add_expense(self):
