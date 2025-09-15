155057a0e5053f9b073b6f539be7d8b0168d6ca4
lthistelt8 <o_lthistel6@protonmail.com>
2025-09-15 00:02:39 -0400

    feat: re-initializing del_expense code block for now. need to practice more with dictionary iteration

--- src/main.py
+++ src/main.py
@@ -22,46 +22,8 @@ def add_expense(category, expense_name, expense_amount):
 print(expenses)
 
 def del_expense():
-    """Delete expenses"""
-    if not expenses or not any(expenses.values()) :
-        print("No expenses found for deletion.")
-        return None
+    """Delete expenses - not yet implemented"""
+    print("Feature not yet implemented: delete expense.")
+    return None
 
-    for e, expense in enumerate(expenses, 1):
-        print(f"{e}. {expense['expense name']}")
 
-    print("Enter the number corresponding to the expense you'd like to delete, or 0 to cancel.")
-    while True:
-        try:
-            didx: int = int(input("> "))
-            if didx == 0:
-                print("Deletion cancelled.")
-                break
-        except ValueError:
-            print("Invalid entry. Enter only the number associated with the expense.")
-            continue
-
-        if not 1<= didx <= len(expenses):
-            print(f"Selection out of range. Please enter 1-{len(expenses)}.")
-            continue
-
-        marked = expenses[didx - 1]
-        while True:
-            print(f"Are you sure you want to delete {marked} (y/n)?")
-            confirm = str(input("> "))
-            if confirm not in ('y', 'n'):
-                print("Invalid entry. Please enter 'y' or 'n'.")
-                continue
-
-            if confirm == 'n':
-                print("Deletion cancelled.")
-                print("Returning to main menu...")
-                break
-
-        try:
-            expenses.pop(marked)
-            print(f"{marked} has been deleted.")
-            print("Returning to main menu...")
-            break
-        except Exception as e:
-            print(f"Exception occurred! {e}")
