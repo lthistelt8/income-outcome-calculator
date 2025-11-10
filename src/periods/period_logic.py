b62b6f6975a842f028cf3b8c998a703574a1ca9c
lthistelt8 <o_lthistel6@protonmail.com>
2025-11-10 00:05:02 -0500

    style: added docstring, comment

--- src/periods/period_logic.py
+++ src/periods/period_logic.py
@@ -8,6 +8,10 @@ from src.periods.period_config import BiweeklyExpense
 from src.expenses import expenses
 
 def validate_due_date(expense_date):
+    """
+    Validates a given expense date.
+    """
+    #may be added as its own code block rather than a function
     if expense_date:
         formatted_date = strftime('%d,%m')
         return formatted_date
