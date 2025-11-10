3ebc6023d193d1c90c863870a0b71c7c08dbe435
lthistelt8 <o_lthistel6@protonmail.com>
2025-11-10 00:02:49 -0500

    feat: renamed branch to reflect correct feature addition; added validation check for entered due dates

--- src/periods/period_logic.py
+++ src/periods/period_logic.py
@@ -3,4 +3,12 @@ Business logic executed to correctly assign and track due dates
 """
 
 from datetime import date, timedelta
+from time import strftime
 from src.periods.period_config import BiweeklyExpense
+from src.expenses import expenses
+
+def validate_due_date(expense_date):
+    if expense_date:
+        formatted_date = strftime('%d,%m')
+        return formatted_date
+    return None
