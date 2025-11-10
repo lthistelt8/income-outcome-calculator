7a9136cd2c9fd2768717cab50b1dad35855310f0
lthistelt8 <o_lthistel6@protonmail.com>
2025-11-09 23:08:23 -0500

    feat: added docstring, left comment with intent to remove when feature is fully implemented

--- src/periods/period_config.py
+++ src/periods/period_config.py
@@ -5,5 +5,10 @@ Configure settings for expense due dates
 from datetime import date
 
 class BiweeklyExpense:
+    """
+    Establishes fixed anchor dates and period days
+    """
+    #ANCHOR_DATE is fixed to the system date for now, but will be
+    #eventually tied to the last payday
     ANCHOR_DATE = date.today()
     PERIOD_DAYS = 14
